ecb4632d17a1e4e434d15172bcbedf31852cb12c
Kelley Montejo <montejokelley@gmail.com>
2025-08-05 19:28:21 -0400

    Update script.js

--- IceCreamWebsite/script.js
+++ IceCreamWebsite/script.js
@@ -1,53 +1,61 @@
-const flavors = [
-  {
-  name: "Honey lavander",
-  type: "Ice Cream",
-  image = "https://lh3.googleusercontent.com/gg-dl/AJfQ9KRQgFxezMVGnwMLfNanpxgns0h0SHTjwdO5C5Fx0M_0AdtSPi-vPmESkeQDv74TCkLfF86r4BuvUoZk0RkL0Rcxz3fur__gVnn-ISPPqcNOswD5_53We5vFjls56bP3iBTeWNMWIugLF58b5jD749XrEzV79Rya_XicCOxBS7L3b1Hwag",
-},
-  
-  {
-    name: "Eggnog",
-    type: "Ice Cream",
-    image = "https://lh3.googleusercontent.com/gg-dl/AJfQ9KRfGltxhIWUlQ9H4f4aknTmQUOo2OJjaIKxBDK6BPKxzo6a78vouML-mZf5LIpw5rwXB9vuElvK9NvtIeFc2LcRxrOLI9AOHTTx9w_zOmUVHm6A11n_qCR8AO4Et9qV6xfTKTlk_6hDUhIxCTmpOO3EDFtsgNe0Brfl-9ogCErtTPlj_g",
-  },
 
+const items = [
   {
-    name: "Balsamic Strawberry Sorbet",
-    type: "Ice Cream",
-    image = "https://lh3.googleusercontent.com/gg-dl/AJfQ9KT-EBfiuHrA5D-iFXlByNaX_YD_cyPyN3sxsU8scZ9pmicpOZzlyNqkeYjcEoFxX6eF2KfPHbehtqZBCEEDb0zM-dRWOD4hvNey5CxvRVyXZ4QFss6OR3vCK33wmq0Pj6W_ZqUqkZ6LUt12z4LUS02z0I8wtwT49sh9ps83Bz2JNejkEg";
+    // Name of the item
+    name: "Honey Lavander",
+    image: "https://icecreamfromscratch.com/wp-content/uploads/2023/07/Lavender-Ice-Cream-1.2-3-735x1102.jpg"
   },
-  
   {
-    name: "Brown butter salted popcorn",
-    type: "Ice Cream",
-    image = "https://lh3.googleusercontent.com/gg-dl/AJfQ9KR_HkPlfmKpGVLB0naxQAcDhtcJ3NCErShjkEc7xYE2ERdsqNz72x20piGJF5Nhc9Kya10niGECTawEMQ2Y5Jzfe7w61q4M11Ivp6vf0vN-8-1-q8vdMmkxjB8CLgL65xO2h5NwkS-kWncJ3LHMTfmQTIZr5Dpg1AvRkBbhcgs104zK",
+    name: "Basil",  // Name of the item
+    image: "https://www.savorysimple.net/wp-content/uploads/2012/06/icecream_2547.jpg"
   },
-
   {
-    name: "Lemon Curd, Biscoff, and Marshmallow",
-    type: "Ice Cream",
-    image = "https://lh3.googleusercontent.com/gg-dl/AJfQ9KQR6SfyDVO12516wcJnchm3q-Ty4itKK5CH4_91LoDW8LHfLR8d2lAz8VZimEB0pBDK1vyo97MOZGYlFzY6z_kjmplnSfvbWk-7zd-8h43CklZLN-86kXbEK4HSHPXVZiKE1MunbK3J5PbCEPzIffDGXVb5AT3EBCA5BumfUo1i5HqT",
+    name: "Penut butter and Jelly",  // Name of the item
+    image: "https://www.browneyedbaker.com/wp-content/uploads/2014/07/peanut-butter-jelly-ice-cream-40-600.jpg"
   },
-
   {
-    name: "Peanut butter and jelly",
-    type: "Ice Cream",
-    image = "https://lh3.googleusercontent.com/gg-dl/AJfQ9KStBOcsSVO8rDhyP2-m6zv5IQie6h4fx6xBlWkWZKeikvAqYPQ4UCr8eS7XLvLJH_9IXO3HbmHVsgDIREn7w_-EqYY-9S1L-gNPqa2M1IpY8J2QKwsJ49n_Cw2KlGJm4lUO12HsN7173Pc4kONILC7iPbnXdrpeB7ixJd1FGlKC5Fqb9w",
+    name: "Balsamic Strawberry Sorbet",  // Name of the item
+    image: "https://www.mythreeseasons.com/wp-content/uploads/2021/07/strawberry-balsamic-ice-cream-1200px-5.jpg"
   },
-
-  {
-    name: "Gooseberry crumble",
-    type: "Ice Cream",
-    image = "https://lh3.googleusercontent.com/gg-dl/AJfQ9KTTKLM4HcfPlelJmc-ya2UMvpIrmkni4myby_bp2xckHfB4ZsXk7XVcrNV-O0X9GceNGJAMYXUJHPpycX86745L31o5egGYa4eGmD6BypBrnhHXzUU3B1gXS0NjnfLY-eaSN4wMBK45msCyNGDuod_PoN_dWl9DzuctjEgMRP4i018mvw",
-  },
-
-  {
-    name: "Basil",
-    type: "Ice Cream",
-    image = "https://lh3.googleusercontent.com/gg-dl/AJfQ9KTJ_oLY3Af1cgM-dV4FHYQQMzUfTsg6L-thQqUFB15xjheW6KwtJtQNfdYGIxRYDO-IBoa-t3oD4E_iNxEOwLivmtlcI3LnLtgFDSTcc1xZ3m58lAViYEJtHCybdG86c_AcSIAMTPkLeoGgmgcsvLhXoKGlGoDGfOU6VXRvhFtKlMHCUw",
-  }
 ];
 
+//  to keep track if player clicked 
 let selectedItem = null;
-let cost = 0;
-if()
+
+function loadItems() {
+  const itemBox = document.getElementById("items");
+  itemBox.innerHTML = "";
+
+  // Go through each item in the items array
+  items.forEach((item, index) => {
+    const div = document.createElement("div");
+    div.classList.add("item");
+    const img = document.createElement("img");
+    img.src = item.image;
+    img.alt = item.name;
+    const label = document.createElement("p");
+    label.textContent = item.name;
+    div.appendChild(img);
+    div.appendChild(label);
+
+    // When this item box is clicked:
+    div.onclick = () => {
+      selectedItem = item;
+      highlightSelected(index);
+    };
+    itemBox.appendChild(div);
+  });
+
+  // Function to highlight which item is selected by the player
+  function highlightSelected(index) {
+    document.querySelectorAll(".item").forEach((el, i) => {
+      el.classList.toggle("selected", i == index);
+    });
+  }
+
+}
+
+// Wait until the HTML page is fully loaded before running the game setup
+document.addEventListener("DOMContentLoaded", () => {
+  loadItems(); 
+});
