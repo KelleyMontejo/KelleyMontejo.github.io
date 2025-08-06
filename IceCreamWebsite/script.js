1ce898a8f7854073750cec147a6424c5f8c091c0
Kelley Montejo <montejokelley@gmail.com>
2025-08-05 21:20:38 -0400

    Update script.js

--- IceCreamWebsite/script.js
+++ IceCreamWebsite/script.js
@@ -1,20 +1,26 @@
 
 const items = [
   {
-    // Name of the item
     name: "Honey Lavander",
+    description: "Ingredients: Common ingredients include milk, cream, sugar (often replaced or augmented by honey), lavender (fresh or dried culinary lavender buds), and sometimes egg yolks for richness and creaminess. Some recipes also use cream cheese or mascarpone for an even silkier texture.",
+    cost: "5.29",
     image: "https://icecreamfromscratch.com/wp-content/uploads/2023/07/Lavender-Ice-Cream-1.2-3-735x1102.jpg"
   },
   {
-    name: "Basil",  // Name of the item
-    image: "https://www.savorysimple.net/wp-content/uploads/2012/06/icecream_2547.jpg"
+    name: "Basil",
+    description: "",
+    cost: "4.25",
+    image: "https://www.gretchensveganbakery.com/wp-content/uploads/2018/09/thai-basil-cropped.jpg"
   },
   {
-    name: "Penut butter and Jelly",  // Name of the item
+    name: "Penut butter and Jelly", 
+    description: "",
+    cost: "6.54",
     image: "https://www.browneyedbaker.com/wp-content/uploads/2014/07/peanut-butter-jelly-ice-cream-40-600.jpg"
   },
   {
-    name: "Balsamic Strawberry Sorbet",  // Name of the item
+    name: "Balsamic Strawberry Sorbet", 
+    cost: "7.29",
     image: "https://www.mythreeseasons.com/wp-content/uploads/2021/07/strawberry-balsamic-ice-cream-1200px-5.jpg"
   },
 ];
@@ -34,14 +40,18 @@ function loadItems() {
     img.src = item.image;
     img.alt = item.name;
     const label = document.createElement("p");
+    const c = document.createElement("p");
     label.textContent = item.name;
     div.appendChild(img);
     div.appendChild(label);
+    
 
     // When this item box is clicked:
     div.onclick = () => {
       selectedItem = item;
       highlightSelected(index);
+      let amt = Number(prompt("How many scoops: "));
+      let container = prompt("Cone or bowl: ");
     };
     itemBox.appendChild(div);
   });
